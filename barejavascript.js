d6592c12c6f6a60a3622298385dba2a37c180708
Xarzie <70147439+Xarzie@users.noreply.github.com>
2020-09-17 09:53:56 +0200

    finalupdate

--- barejavascript.js
+++ barejavascript.js
@@ -22,10 +22,10 @@ function toggleNavPanel(x) {
     var panel = document.getElementById(x), navarrow = document.getElementById("navarrow"), maxH = "600px"; /* max længde af container skal være større end Css value */
     if (panel.style.height == maxH) {
         panel.style.height = "0px";
-        navarrow.innerHTML = "&#5167";
+        navarrow.innerHTML = "&#9776";
     } else {
         panel.style.height = maxH;
-        navarrow.innerHTML = "&#5169";
+        navarrow.innerHTML = "&#9776";
     }
 }
 
